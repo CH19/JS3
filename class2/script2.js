53a3964294c1cc66b2ab07a887eb0a6ad5b6b660
christian <chrislacruz2005@gmail.com>
2023-05-09 15:20:29 -0400

    se finalizo la clase 2

--- class2/script2.js
+++ class2/script2.js
@@ -1,37 +1,48 @@
-const form = document.querySelector('form');
-let inputs =  document.querySelectorAll('.form-control');
-const input1 = document.querySelector('.user');
-let xd = Array.from(inputs);
+// const form = document.querySelector('form');
+// let inputs =  document.querySelectorAll('.form-control');
+// const input1 = document.querySelector('.user');
+// let xd = Array.from(inputs);
 
-function Validacion(){
-  // .preventDefault(); // evita el envío del formulario por defecto
-  const InputsValue = xd.map(e => e[value]);
-  console.log(inputs);
-  console.log(xd);
-  console.log(InputsValue);
-}
-// validar = () =>{
-//     const formu = document.querySelector('form');
-//   const input = formu.querySelectorAll('input');
+// function Validacion(){
+//   // .preventDefault(); // evita el envío del formulario por defecto
+//   const InputsValue = xd.map(e => e[value]);
+//   console.log(inputs);
+//   console.log(xd);
+//   console.log(InputsValue);
+// }
+// // validar = () =>{
+// //     const formu = document.querySelector('form');
+// //   const input = formu.querySelectorAll('input');
   
-//   const values = {};
-//   input.forEach(input => {
-//     values[input.name] = input.value;
-//   });
-//   console.log('click')
-//   console.log(values);
+// //   const values = {};
+// //   input.forEach(input => {
+// //     values[input.name] = input.value;
+// //   });
+// //   console.log('click')
+// //   console.log(values);
+// // }
+
+// function ControlForm(){
+//   if (confirm('formulario enviado')) {
+//     alert('gols' + typeof(phone.value));
+//   } else {
+//     return;
+//   }
 // }
 
-function ControlForm(){
-  if (confirm('formulario enviado')) {
-    alert('gols' + typeof(phone.value));
-  } else {
-    return;
+// const axd = ()=>{
+//   console.log(input1);
+//   console.log(input1.value);
+// }
+//Se comento porque el script que hace algo ya esta en el archivo
+
+//Script de validar checkbox
+
+const ValidarCheckbox = ()=>{
+  if(!form.checkbox){
+    alert('debes aceptar los terminos y condiciones')
+    return false
+  }else{
+    return true;
   }
 }
-
-const axd = ()=>{
-  console.log(input1);
-  console.log(input1.value);
-}
-
